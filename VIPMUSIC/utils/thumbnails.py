d063edaf6ef788c9166e4c0581124dc30473413d
LeoTeto <166168826+LeoTeto@users.noreply.github.com>
2024-04-16 11:31:14 +0200

    Update thumbnails.py

--- VIPMUSIC/utils/thumbnails.py
+++ VIPMUSIC/utils/thumbnails.py
@@ -1,10 +1,16 @@
 import os
 import re
+import random
+
 import aiofiles
 import aiohttp
-from PIL import Image, ImageDraw, ImageEnhance, ImageFilter, ImageFont, ImageOps
+
+from PIL import Image, ImageDraw, ImageEnhance
+from PIL import ImageFilter, ImageFont, ImageOps
+
 from unidecode import unidecode
 from youtubesearchpython.__future__ import VideosSearch
+
 from VIPMUSIC import app
 from config import YOUTUBE_IMG_URL
 
